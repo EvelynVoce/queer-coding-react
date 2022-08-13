ab35b5a4166ce46d2b040479fd40943cc2d14175
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-08-13 15:04:21 +0100

    Added custom css and updated main page HTML

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,19 @@
 ﻿import React from "react";
-
 export const MainPage = () => {
     return (
-        <div className="d-flex justify-content-center">
-            <h1>QueerCoding</h1>
+        <div className="my-3">
+            
+            <h1 className="d-flex justify-content-center"><u>QueerCoding</u></h1>
+            <div className="row my-3">
+                <div className="col-md-6 d-flex justify-content-center">
+                    <p>temp</p>
+                </div>
+                <div className="col-md-6 d-flex justify-content-center">
+                    <p className="main-font">QueerCoding's goal is to educate and inspire members of the LGBTQ+ community to take
+                        their first step into tech. Providing them with the skills and confidence to
+                        begin their journey become developers and data analysts.</p>
+                </div>
+            </div>
         </div>
     );
 }
